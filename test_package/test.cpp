81ae58770626aefb6df00d6cc89bfbca242e1806
Trung Pham <phambaotrung@gmail.com>
2017-04-15 07:15:09 -0700

    Build on Windows

--- test_package/test.cpp
+++ test_package/test.cpp
@@ -1,8 +1,4 @@
 #include <iostream>
-#include "apr-1/apr_general.h"
 
 int main(int argc, char* argv[]) {
-    const char* const* test = argv;
-    apr_app_initialize(&argc, &test, 0);
-    apr_terminate();
 }
