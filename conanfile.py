7b52e99c64e93a46264463056def8d5d6dc53343
Trung Pham <phambaotrung@gmail.com>
2017-04-24 18:36:22 -0700

    Use latest trunk

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 class ApacheaprConan(ConanFile):
     description = "Apache Portable Runtime"
     name = "apache-apr"
-    version = "1.6.0"
+    version = "latest"
     license = "Apache-2.0"
     url = "https://github.com/pbtrung/conan-apache-apr"
     settings = "os", "compiler", "build_type", "arch"
@@ -13,13 +13,14 @@ class ApacheaprConan(ConanFile):
     install_dir = "install"
 
     def source(self):
-        zip_name = "apr-%s.tar.gz" % self.version
-        tools.download("https://codeload.github.com/apache/apr/tar.gz/%s" % self.version, zip_name, verify=False)
+        zip_name = "apr-%s.zip" % self.version
+        tools.download("https://github.com/apache/apr/archive/trunk.zip" % self.version, zip_name, verify=False)
         tools.unzip(zip_name)
         os.unlink(zip_name)
 
     def configure(self):
-        self.requires.add("libtool/2.4.6@sztomi/testing", private=False, dev=True)
+        if self.settings.os != "Windows":
+            self.requires.add("libtool/2.4.6@sztomi/testing", private=False)
 
     def build(self):
         env_build = AutoToolsBuildEnvironment(self)
@@ -36,25 +37,25 @@ class ApacheaprConan(ConanFile):
             
     def package(self):
         install_path = self.install_dir + os.sep
-        self.copy("*.h", dst="include/apr-1", src=install_path + "include/apr-1", keep_path=False)
+        self.copy("*.h", dst="include/apr-2", src=install_path + "include/apr-2", keep_path=False)
         if self.settings.os != "Windows":
             self.copy("*.pc", dst="lib/pkgconfig", src=install_path + "lib", keep_path=False)
             if self.options.shared:
-                self.copy("*.so*", dst="lib", src=install_path + "lib", keep_path=False)
-                self.copy("*.dylib", dst="lib", src=install_path + "lib", keep_path=False)
+                self.copy("*.so*", dst="lib", src=install_path + "lib", keep_path=True)
+                self.copy("*.dylib", dst="lib", src=install_path + "lib", keep_path=True)
             else:
-                self.copy("*.a", dst="lib", src=install_path + "lib", keep_path=False)
+                self.copy("*.a", dst="lib", src=install_path + "lib", keep_path=True)
         else:
             if self.options.shared:
-                self.copy("*.dll", dst="bin", src=install_path, keep_path=False)
+                self.copy("*.dll", dst="bin", src=install_path, keep_path=True)
             else:
-                self.copy("*.lib", dst="lib", src=install_path, keep_path=False)
+                self.copy("*.lib", dst="lib", src=install_path, keep_path=True)
 
-        self.copy("apr-1-config", dst="bin", src=install_path + "bin", keep_path=False)
+        self.copy("apr-2-config", dst="bin", src=install_path + "bin", keep_path=False)
         self.copy("*.exp", dst="lib", src=install_path, keep_path=False)
-        self.copy("*", dst="build-1", src=install_path + "build-1", keep_path=False)
+        self.copy("*", dst="build-2", src=install_path + "build-2", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.includedirs = ["include", "include/apr-1"]
+        self.cpp_info.includedirs = ["include", "include/apr-2"]
         self.cpp_info.bindirs = ["bin"]
-        self.cpp_info.libs = ["apr-1"]
+        self.cpp_info.libs = ["apr-2"]
